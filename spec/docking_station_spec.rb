eb1eb9ff77ac5bf3f649e64ec2e5edf5e4ba1d1d
Sha <cspoppuppy@hotmail.com>
2021-03-02 15:56:53 +0000

    pass first class method test
    Co-authored-by: Lou Reade <lou.m.reade@gmail.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+    # it "respond to method" do 
+    #     is_expected.to respond_to(:release_bike)
+    # end
 
+    it { is_expected.to respond_to(:release_bike)}
 end

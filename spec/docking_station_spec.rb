c416aef3940bc4ee2e02caa76b3458f1e9da1621
Sha <cspoppuppy@hotmail.com>
2021-03-02 16:47:10 +0000

    release_bike and Bike.working? plus tests
    Co-authored-by: Lou Reade <lou.m.reade@gmail.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
     # it "respond to method" do 
@@ -6,4 +7,15 @@ describe DockingStation do
     # end
 
     it { is_expected.to respond_to(:release_bike)}
+
+    docking_station = DockingStation.new
+    bikeInstance = docking_station.release_bike
+
+    it "realse_bike returns a bike" do
+        expect(bikeInstance).to be_a Bike
+    end
+
+    it "check if bike working" do
+        expect(bikeInstance.working?).to eq true
+    end
 end
